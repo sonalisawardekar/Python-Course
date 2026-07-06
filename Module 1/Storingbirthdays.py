deab07b963bb9a99dcdbce632d05fdf21852ce93
sonalisawardekar <sonali.sawar@gmail.com>
2026-07-06 23:56:08 +0530

    initial commit

---
+++ Module 1/Storingbirthdays.py
@@ -0,0 +1,14 @@
+# Storing birthday dates in different variables
+
+birthday1 = "12-01-2010"
+birthday2 = "25-03-2011"
+birthday3 = "18-07-2009"
+birthday4 = "09-11-2012"
+birthday5 = "30-12-2010"
+
+# Displaying the birthday dates
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
