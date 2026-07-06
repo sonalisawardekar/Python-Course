5d87992e1428c75987a51a0879fea5975eddec52
sonalisawardekar <sonali.sawar@gmail.com>
2026-07-07 00:18:12 +0530

    initial commit

---
+++ Congratulations.py
@@ -0,0 +1,8 @@
+# Input a word in lowercase
+word = input("Enter a word in lowercase: ")
+
+# Convert the word to uppercase
+uppercase_word = word.upper()
+
+# Display the result
+print("Word in uppercase:", uppercase_word)
